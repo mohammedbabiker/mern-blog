c4664e46a9ef71460c193d1f0390ed6f1c932bfb
Mohammed Babiker <mobabikerbabai20@gmail.com>
2024-04-04 06:08:08 +0300

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main className="p-10 max-w-[900px] m-0-auto ">
+        <header className=" flex justify-between">
+          <a href="#" className="">
+            Mohammed Blog
+          </a>
+          <nav>
+            <a href="#" className="">
+              Login
+            </a>
+            <a href="#" className="">
+              Register
+            </a>
+          </nav>
+        </header>
+      </main>
+    </>
+  );
+}
+
+export default App;
