a28948fc1c47d39814f9910f0a6d48bafe57d337
Mohammed Babiker <mobabikerbabai20@gmail.com>
2024-04-04 20:14:36 +0300

    Login page added

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,3 +1,23 @@
 export default function LoginPage() {
-  return <div>Login</div>;
+  return (
+    <form className="max-w-md my-0 mx-auto">
+      <h1 className="text-3xl font-bold mb-5 text-center">Login</h1>
+      <input
+        type="text"
+        placeholder="Username"
+        className="block mb-3 w-full py-1 px-3 border border-gray-300 rounded-md"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        className="block mb-3 w-full py-1 px-3 border border-gray-300 rounded-md"
+      />
+      <button
+        type="submit"
+        className="w-full bg-gray-500 border-none rounded-md text-white p-1"
+      >
+        Login
+      </button>
+    </form>
+  );
 }
