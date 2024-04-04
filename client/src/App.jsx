2746c6b1366ba8f11a613027ca7bde63419ded1b
Mohammed Babiker <mobabikerbabai20@gmail.com>
2024-04-04 07:30:27 +0300

    Homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,12 @@ import "./App.css";
 function App() {
   return (
     <>
-      <main className="p-10 max-w-[900px] m-0-auto ">
-        <header className=" flex justify-between">
-          <a href="#" className="">
+      <main className="p-10 max-w-[900px] m-0 mx-auto">
+        <header className=" flex justify-between mb-8 items-center">
+          <a href="#" className="no-underline text-inherit font-bold text-xl">
             Mohammed Blog
           </a>
-          <nav>
+          <nav className="flex gap-4">
             <a href="#" className="">
               Login
             </a>
@@ -17,6 +17,32 @@ function App() {
             </a>
           </nav>
         </header>
+        <div className="grid grid-cols-2 gap-5 mb-4">
+          <div className="">
+            <img
+              src="https://picsum.photos/900/300"
+              alt="post-image"
+              className="max-w-full"
+            />
+          </div>
+          <div className="">
+            <h1 className="font-bold m-0 text-xl">
+              Whispers of Time: Echoes from Forgotten Dreams and Distant
+              Memories
+            </h1>
+            <p className="text-sm my-[6px] mx-0 flex gap-3">
+              <a href="#" className="text-gray-500">
+                Babai
+              </a>
+              <time className="text-gray-500">2024-04-04 16:45</time>
+            </p>
+            <p className="my-2 mx-0 leading-5">
+              In the midst of swirling mist, shadows danced, whispering secrets
+              lost to time. Each breath carried echoes of forgotten tales,
+              lingering in the air like gentle melodies.
+            </p>
+          </div>
+        </div>
       </main>
     </>
   );
