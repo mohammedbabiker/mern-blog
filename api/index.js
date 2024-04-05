ba33885848d5f28b4d77635acd8f0d4ad713ee7a
Mohammed Babiker <mobabikerbabai20@gmail.com>
2024-04-05 20:03:17 +0300

    Edit post

--- api/index.js
+++ api/index.js
@@ -100,6 +100,35 @@ app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
   });
 });
 
+app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
+  let newPath = null;
+  if (req.file) {
+    const { originalname, path } = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { title, summary, content, id } = req.body;
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json("You are not the author of this post");
+    }
+    await postDoc.updateOne({
+      title,
+      summary,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
+    });
+    res.json(postDoc);
+  });
+});
+
 app.get("/post", async (req, res) => {
   const posts = await Post.find()
     .populate("author", "username")
---
+++ client/src/pages/EditPost.jsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+import { Navigate, useParams } from "react-router-dom";
+import Editor from "../Editor";
+
+export default function EditPost() {
+  const { id } = useParams();
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/post/" + id).then((response) => {
+      response.json().then((postInfo) => {
+        setTitle(postInfo.title);
+        setSummary(postInfo.summary);
+        setContent(postInfo.content);
+      });
+    });
+  }, [id]);
+
+  async function updatePost(ev) {
+    ev.preventDefault();
+
+    const data = new FormData();
+
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set("id", id);
+    if (files?.[0]) {
+      data.set("file", files[0]);
+    }
+
+    const response = await fetch("http://localhost:4000/post/", {
+      method: "PUT",
+      body: data,
+      credentials: "include",
+    });
+    if (response.ok) {
+      setRedirect(true);
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={"/post/" + id} />;
+  }
+  return (
+    <form className="max-w-md my-0 mx-auto" onSubmit={updatePost}>
+      <input
+        type="text"
+        placeholder="Title"
+        className="block mb-3 w-full py-1 px-3 border border-gray-300 rounded-md"
+        value={title}
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Summary"
+        className="block mb-3 w-full py-1 px-3 border border-gray-300 rounded-md"
+        value={summary}
+        onChange={(ev) => setSummary(ev.target.value)}
+      />
+      <input
+        type="file"
+        className="block mb-3 w-full py-1 px-3 border border-gray-300 rounded-md"
+        // value={files}
+        onChange={(ev) => setFiles(ev.target.files)}
+      />
+      <Editor value={content} onChange={setContent} />
+      <button className="w-full bg-gray-500 border-none rounded-md text-white p-1">
+        Update Post
+      </button>
+    </form>
+  );
+}
