43b4d699ee5a58b166aa6afef18a0e8423be995e
Mohammed Babiker <mobabikerbabai20@gmail.com>
2024-04-05 20:03:42 +0300

    Updates

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -29,14 +29,32 @@ export default function Header() {
       <Link to="/" className="no-underline text-inherit font-bold text-xl">
         Mohammed Blog
       </Link>
-      <nav className="flex gap-4">
+      <nav className="flex gap-4 items-center">
         {username && (
           <>
+            <span> Welcome, {username}</span>
             <Link to="/create" className="">
               Create new Post
             </Link>
-            <Link onClick={logout} className="">
+            <Link
+              onClick={logout}
+              className="inline-flex gap-1 bg-gray-700 text-white rounded-[5px] px-2 py-1 items-center "
+            >
               Logout
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className=" h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
+                />
+              </svg>
             </Link>
           </>
         )}
