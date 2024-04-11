304f8b73362cc1f6efa757351eef1c402ba2924e
Mohammed Babiker <mobabikerbabai20@gmail.com>
2024-04-11 20:32:49 +0300

    Use try catch

--- api/index.js
+++ api/index.js
@@ -49,32 +49,44 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  const userDoc = await User.findOne({ username });
-  const passOk = bcrypt.compareSync(password, userDoc.password); // true or false
-  if (passOk) {
-    // login successfully
-    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
-      if (err) throw err;
-      res.cookie("token", token).json({
-        id: userDoc._id,
-        username,
+  try {
+    const userDoc = await User.findOne({ username });
+    const passOk = bcrypt.compareSync(password, userDoc.password); // true or false
+    if (passOk) {
+      // login successfully
+      jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+        if (err) throw err;
+        res.cookie("token", token).json({
+          id: userDoc._id,
+          username,
+        });
       });
-    });
-  } else {
-    res.status(401).json({ message: "Login failed" });
+    } else {
+      res.status(401).json({ message: "Login failed" });
+    }
+  } catch (e) {
+    return res.status(401).json({ message: e });
   }
 });
 
 app.get("/profile", (req, res) => {
-  const { token } = req.cookies;
-  jwt.verify(token, secret, {}, (err, info) => {
-    if (err) throw err;
-    res.json(info);
-  });
+  try {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+      if (err) throw err;
+      res.json(info);
+    });
+  } catch (e) {
+    return res.status(401).json({ message: e });
+  }
 });
 
 app.post("/logout", (req, res) => {
-  res.cookie("token", "").json("ok");
+  try {
+    res.cookie("token", "").json("ok");
+  } catch (e) {
+    return res.status(401).json({ message: e });
+  }
 });
 
 app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
@@ -83,63 +95,86 @@ app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
   const ext = parts[parts.length - 1];
   const newPath = path + "." + ext;
   fs.renameSync(path, newPath);
-
-  const { token } = req.cookies;
-  jwt.verify(token, secret, {}, async (err, info) => {
-    if (err) throw err;
-    const { title, summary, content } = req.body;
-    const postDoc = await Post.create({
-      title,
-      summary,
-      content,
-      cover: newPath,
-      author: info.id,
+  try {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+      if (err) throw err;
+      const { title, summary, content } = req.body;
+      const postDoc = await Post.create({
+        title,
+        summary,
+        content,
+        cover: newPath,
+        author: info.id,
+      });
+      res.json(postDoc);
     });
-    res.json(postDoc);
-  });
+  } catch (e) {
+    return res.status(401).json({ message: e });
+  }
 });
 
 app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
   let newPath = null;
-  if (req.file) {
-    const { originalname, path } = req.file;
-    const parts = originalname.split(".");
-    const ext = parts[parts.length - 1];
-    newPath = path + "." + ext;
-    fs.renameSync(path, newPath);
-  }
-
-  const { token } = req.cookies;
-  jwt.verify(token, secret, {}, async (err, info) => {
-    if (err) throw err;
-    const { title, summary, content, id } = req.body;
-    const postDoc = await Post.findById(id);
-    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
-    if (!isAuthor) {
-      return res.status(400).json("You are not the author of this post");
+  try {
+    if (req.file) {
+      const { originalname, path } = req.file;
+      const parts = originalname.split(".");
+      const ext = parts[parts.length - 1];
+      newPath = path + "." + ext;
+      fs.renameSync(path, newPath);
     }
-    await postDoc.updateOne({
-      title,
-      summary,
-      content,
-      cover: newPath ? newPath : postDoc.cover,
+  } catch (e) {
+    res.status(400).json({ message: e });
+    return;
+  }
+  try {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+      if (err) throw err;
+      const { title, summary, content, id } = req.body;
+      const postDoc = await Post.findById(id);
+      const isAuthor =
+        JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+      if (!isAuthor) {
+        return res.status(400).json("You are not the author of this post");
+      }
+      await postDoc.updateOne({
+        title,
+        summary,
+        content,
+        cover: newPath ? newPath : postDoc.cover,
+      });
+      res.json(postDoc);
     });
-    res.json(postDoc);
-  });
+  } catch (e) {
+    res.status(401).json({ message: e });
+    return;
+  }
 });
 
 app.get("/post", async (req, res) => {
-  const posts = await Post.find()
-    .populate("author", "username")
-    .sort({ createdAt: -1 })
-    .limit(17);
-  res.json(posts);
+  try {
+    const posts = await Post.find()
+      .populate("author", "username")
+      .sort({ createdAt: -1 })
+      .limit(17);
+    res.json(posts);
+  } catch (e) {
+    res.status(400).json({ message: e });
+    return;
+  }
 });
 
 app.get("/post/:id", async (req, res) => {
-  const { id } = req.params;
-  const postDoc = await Post.findById(id).populate("author", "username");
-  res.json(postDoc);
+  try {
+    const { id } = req.params;
+    const postDoc = await Post.findById(id).populate("author", "username");
+    res.json(postDoc);
+  } catch (e) {
+    res.status(400).json({ message: e });
+    return;
+  }
 });
 
 app.listen(process.env.port, () => {
